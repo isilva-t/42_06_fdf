38ca9fd071bc24520ce44c11a33d2328c9e56e4b
isilva-t <ivanrfteixeira@gmail.com>
2024-07-11 11:54:35 +0100

    makefile configs

---
+++ fdf.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isilva-t <isilva-t@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/11 09:21:19 by isilva-t          #+#    #+#             */
+/*   Updated: 2024/07/11 11:07:13 by isilva-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+
+
+#include "minilibx-linux/mlx.h"
+#include <X11/keysym.h>
+#include "./libs/libft/libft.h"
+#include "./libs/ft_printf/ft_printf.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#include <stdio.h> //remove before submission
+
+
+
+
+
+#endif
